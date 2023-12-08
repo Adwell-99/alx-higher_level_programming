d320684ac73ffb0147fe3d1e48b2a63b7a92eaab
Adwell-99 <adelekeopeyemi2000@gmail.com>
2023-12-08 04:27:41 -0800

    Updated

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-from functools import reduce
-
-def find_top_score(dictionary):
-    if not dictionary:
+def best_score(a_dictionary):
+    if not a_dictionary:
         return None
 
-    return reduce(lambda x, y: x if dictionary[x] > dictionary[y] else y, dictionary)
+    max_key = max(a_dictionary, key=a_dictionary.get)
+    return max_key
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
-i#!/usr/bin/python3
-
-def display_sorted_dictionary(dictionary):
-    for key, value in sorted(dictionary.items()):
-        print("{}: {}".format(key, value))
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    list_ord = list(a_dictionary.keys())
+    list_ord.sort()
+    for i in list_ord:
+        print("{}: {}".format(i, a_dictionary.get(i)))
